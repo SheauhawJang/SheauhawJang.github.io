eddf667740d787d3bb6a7675945d0592bfe1b10b
SheauhawJang <www.zhangxiaohao.cn@foxmail.com>
2025-05-24 12:46:13 +0800

    Site updated: 2025-05-24 12:46:13

--- old_blogs/tools/mahjong-worker-lang.js
+++ old_blogs/tools/mahjong-worker-lang.js
@@ -80,7 +80,7 @@ const en_loc = {
     da: "Discard",
     hepai: "Win",
     tingpai: "Ready",
-    xiangting: "Steps to win",
+    xiangting: "steps to win",
     hepaichaijie: "Winning tiles division",
     windvd_else_head: "And other",
     windvd_else_tail: "winning tiles dividing ways.",

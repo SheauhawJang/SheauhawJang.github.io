7cd992bdf6b6a73fd0d9551c1890012170853066
SheauhawJang <www.zhangxiaohao.cn@foxmail.com>
2025-05-25 03:41:46 +0800

    Site updated: 2025-05-25 03:41:46

--- old_blogs/tools/mahjong-worker.js
+++ old_blogs/tools/mahjong-worker.js
@@ -20,16 +20,16 @@ function printWaiting(tiles, tcnt, full_tcnt, subtiles, getWaiting, getSubchecks
             const cnt = gcnt + bcnt;
             const ratio = (gcnt / cnt) * 100;
             result +=
-                `<td class="waiting-brief">${loc.dai} ${cnt} ${loc.mei}</td>` +
+                `<td class="waiting-brief">${loc.wait} ${cnt} ${loc.counts}</td>` +
                 `<td class="devided-waiting-td">` +
-                `<div style="display: flex; white-space: nowrap;"><div class="devided-waiting-brief">${loc.haoxing} ${gcnt} ${loc.mei}</div>` +
+                `<div style="display: flex; white-space: nowrap;"><div class="devided-waiting-brief">${loc.goodshape} ${gcnt} ${loc.counts}</div>` +
                 `<div class="devided-waiting-cards">${gans.map(cardImage).join("")}</div></div>` +
-                `<div style="display: flex; white-space: nowrap;"><div class="devided-waiting-brief">${loc.yuxing} ${bcnt} ${loc.mei}</div>` +
+                `<div style="display: flex; white-space: nowrap;"><div class="devided-waiting-brief">${loc.badshape} ${bcnt} ${loc.counts}</div>` +
                 `<div class="devided-waiting-cards">${ans.map(cardImage).join("")}</div></div>` +
-                `<div class="devided-waiting-brief">${loc.haoxinglyu} ${ratio.toFixed(2)}%</div></td>`;
+                `<div class="devided-waiting-brief">${loc.goodshaperate} ${ratio.toFixed(2)}%</div></td>`;
         } else {
             const cnt = CountWaitingCards(tiles, subtiles, ans);
-            result += `<td class="waiting-brief">${loc.dai} ${cnt} ${loc.mei}</td><td style="padding-left: 10px;">${ans.map(cardImage).join("")}</td>`;
+            result += `<td class="waiting-brief">${loc.wait} ${cnt} ${loc.counts}</td><td style="padding-left: 10px;">${ans.map(cardImage).join("")}</td>`;
         }
         return { output: table_head + result + table_tail, ans: { waiting: save } };
     } else {
@@ -62,25 +62,25 @@ function printWaiting(tiles, tcnt, full_tcnt, subtiles, getWaiting, getSubchecks
             if (gcnt !== undefined) {
                 const ratio = (gcnt / cnt) * 100;
                 result +=
-                    `<tr><td class="waiting-brief">${verb} ${cardImage(id)} ${loc.dai} ${cnt} ${loc.mei}</td>` +
+                    `<tr><td class="waiting-brief">${verb} ${cardImage(id)} ${loc.wait} ${cnt} ${loc.counts}</td>` +
                     `<td class="devided-waiting-td">` +
-                    `<div style="display: flex; white-space: nowrap;"><div class="devided-waiting-brief">${loc.haoxing} ${gcnt} ${loc.mei}</div>` +
+                    `<div style="display: flex; white-space: nowrap;"><div class="devided-waiting-brief">${loc.goodshape} ${gcnt} ${loc.counts}</div>` +
                     `<div class="devided-waiting-cards">${save[id].gans.map(cardImage).join("")}</div></div>` +
-                    `<div style="display: flex; white-space: nowrap;"><div class="devided-waiting-brief">${loc.yuxing} ${bcnt} ${loc.mei}</div>` +
+                    `<div style="display: flex; white-space: nowrap;"><div class="devided-waiting-brief">${loc.badshape} ${bcnt} ${loc.counts}</div>` +
                     `<div class="devided-waiting-cards">${save[id].ans.map(cardImage).join("")}</div></div>` +
-                    `<div class="devided-waiting-brief">${loc.haoxinglyu} ${ratio.toFixed(2)}%</div></td></tr>`;
+                    `<div class="devided-waiting-brief">${loc.goodshaperate} ${ratio.toFixed(2)}%</div></td></tr>`;
             } else
                 result +=
-                    `<tr><td class="waiting-brief">${verb} ${cardImage(id)} ${loc.dai} ${cnt} ${loc.mei}</td>` +
+                    `<tr><td class="waiting-brief">${verb} ${cardImage(id)} ${loc.wait} ${cnt} ${loc.counts}</td>` +
                     `<td style="padding-left: 10px;">${save[id].ans.map(cardImage).join("")}</td></tr>`;
         }
         return { output: table_head + result + table_tail, ans: { waiting: save, subchecks } };
     }
 }
 function getWaitingType(step) {
-    if (step === -1) return loc.hepai;
-    else if (step === 0) return loc.tingpai;
-    else return step + " " + loc.xiangting;
+    if (step === -1) return loc.win;
+    else if (step === 0) return loc.ready;
+    else return step + " " + loc.steps;
 }
 function getWinningLine(cards) {
     return cards.map((b) => b.map(cardImageDivide).join("")).join(divideSpace);
@@ -93,7 +93,7 @@ function normalStep(tiles, tcnt, full_tcnt, subtiles) {
     if (step === -1 && full_tcnt > 0) {
         dvd = windvd(tiles, full_tcnt);
         const ots = WinOutput(tiles, full_tcnt, dvd.dvd, 10);
-        output += loc.hepaichaijie + ": \n";
+        output += loc.windvd + ": \n";
         output += ots.map((a) => `<div class="card-container">${getWinningLine(a)}</div>`).join("");
         if (ots.length < dvd.cnt) output += `${loc.windvd_else_head} ${dvd.cnt - ots.length} ${loc.windvd_else_tail}`;
     }
@@ -111,26 +111,26 @@ function normalStep(tiles, tcnt, full_tcnt, subtiles) {
 function JPStep(tiles, tcnt, full_tcnt, subtiles, subcnt, dvd) {
     let table = "";
     let step4 = Step(tiles, tcnt, full_tcnt, 4);
-    table += `<tr><td style="padding-left: 0px;">${loc.yibanxing}${loc.colon}</td><td>${getWaitingType(step4)}</td></tr>`;
+    table += `<tr><td style="padding-left: 0px;">${loc.normal}${loc.colon}</td><td>${getWaitingType(step4)}</td></tr>`;
     postMessage({ output: table_head + table + table_tail });
     let stepJP = step4;
     let step7 = Infinity, step13 = Infinity;
     if (full_tcnt === 14 && subcnt === 0) {
         step7 = PairStep(tiles, true);
-        table += `<tr><td style="padding-left: 0px;">${loc.qiduixing}${loc.colon}</td><td>${getWaitingType(step7)}</td></tr>`;
+        table += `<tr><td style="padding-left: 0px;">${loc.pair7}${loc.colon}</td><td>${getWaitingType(step7)}</td></tr>`;
         postMessage({ output: table_head + table + table_tail });
         stepJP = Math.min(stepJP, step7);
         step13 = OrphanStep(tiles);
-        table += `<tr><td style="padding-left: 0px;">${loc.guoshiwushuangxing}${loc.colon}</td><td>${getWaitingType(step13)}</td></tr>`;
+        table += `<tr><td style="padding-left: 0px;">${loc.kokushi}${loc.colon}</td><td>${getWaitingType(step13)}</td></tr>`;
         postMessage({ output: table_head + table + table_tail });
         stepJP = Math.min(stepJP, step13);
     }
     let output = getWaitingType(stepJP);
     let stepTypeJP = [];
-    if (step4 == stepJP) stepTypeJP.push(loc.yibanxing);
-    if (step7 === stepJP) stepTypeJP.push(loc.qiduixing);
-    if (step13 === stepJP) stepTypeJP.push(loc.guoshiwushuangxing);
-    output += ` （` + stepTypeJP.join("／") + `）\n`;
+    if (step4 == stepJP) stepTypeJP.push(loc.normal);
+    if (step7 === stepJP) stepTypeJP.push(loc.pair7);
+    if (step13 === stepJP) stepTypeJP.push(loc.kokushi);
+    output += ` ${loc.brace_left}${stepTypeJP.join(loc.slash)}${loc.brace_right}\n`;
     let brief = output;
     output += table_head + table + table_tail;
     postMessage({ output, brief });
@@ -142,19 +142,19 @@ function JPStep(tiles, tcnt, full_tcnt, subtiles, subcnt, dvd) {
         if (step7 === -1) {
             ++cnt;
             dvd7 = PairOutput(tiles);
-            odvd.push(`<div class="waiting-brief">${loc.qiduixing}${loc.colon}</div><div class="card-container">${getWinningLine(dvd7)}</div>`);
+            odvd.push(`<div class="waiting-brief">${loc.pair7}${loc.colon}</div><div class="card-container">${getWinningLine(dvd7)}</div>`);
         }
         if (step13 === -1) {
             ++cnt;
             dvd13 = OrphanOutput(tiles);
-            odvd.push(`<div class="waiting-brief">${loc.guoshiwushuangxing}${loc.colon}</div><div class="card-container">${getWinningLine(dvd13)}</div>`);
+            odvd.push(`<div class="waiting-brief">${loc.kokushi}${loc.colon}</div><div class="card-container">${getWinningLine(dvd13)}</div>`);
         }
         if (step4 === -1) {
             cnt += dvd.cnt;
             const ots = WinOutput(tiles, full_tcnt, dvd.dvd, 10 - odvd.length);
-            odvd = [...ots.map((a) => `<div class="waiting-brief">${loc.yibanxing}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`), ...odvd];
+            odvd = [...ots.map((a) => `<div class="waiting-brief">${loc.normal}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`), ...odvd];
         }
-        output += loc.hepaichaijie + ": \n";
+        output += loc.windvd + ": \n";
         output += `<div class="win-grid">${odvd.join("")}</div>`;
         if (odvd.length < cnt) output += `${loc.windvd_else_head} ${cnt - odvd.length} ${loc.windvd_else_tail}`;
     }
@@ -172,13 +172,13 @@ function JPStep(tiles, tcnt, full_tcnt, subtiles, subcnt, dvd) {
 function GBStep(tiles, tcnt, full_tcnt, subtiles, subcnt, step, save, step13, dvd, dvd7, dvd13) {
     let table = "";
     let stepGB = step;
-    table += `<tr><td style="padding-left: 0px;">${loc.yibanxing}${loc.colon}</td><td>${getWaitingType(step)}</td></tr>`;
+    table += `<tr><td style="padding-left: 0px;">${loc.normal}${loc.colon}</td><td>${getWaitingType(step)}</td></tr>`;
     postMessage({ output: table_head + table + table_tail });
     let step7 = Infinity, step16 = Infinity, stepkd = Infinity;
     if (full_tcnt === 14 && subcnt === 0) {
         step7 = PairStep(tiles, false);
-        table += `<tr><td style="padding-left: 0px;">${loc.qiduixing}${loc.colon}</td><td>${getWaitingType(step7)}</td></tr>`;
-        table += `<tr><td style="padding-left: 0px;">${loc.shisanyaoxing}${loc.colon}</td><td>${getWaitingType(step13)}</td></tr>`;
+        table += `<tr><td style="padding-left: 0px;">${loc.pair7}${loc.colon}</td><td>${getWaitingType(step7)}</td></tr>`;
+        table += `<tr><td style="padding-left: 0px;">${loc.orphan13}${loc.colon}</td><td>${getWaitingType(step13)}</td></tr>`;
         postMessage({ output: table_head + table + table_tail });
         stepGB = Math.min(stepGB, step7);
         stepGB = Math.min(stepGB, step13);
@@ -195,12 +195,12 @@ function GBStep(tiles, tcnt, full_tcnt, subtiles, subcnt, step, save, step13, dv
     }
     let output = getWaitingType(stepGB);
     let stepTypeGB = [];
-    if (step == stepGB) stepTypeGB.push(loc.yibanxing);
-    if (step7 === stepGB) stepTypeGB.push(loc.qiduixing);
-    if (step13 === stepGB) stepTypeGB.push(loc.shisanyaoxing);
+    if (step == stepGB) stepTypeGB.push(loc.normal);
+    if (step7 === stepGB) stepTypeGB.push(loc.pair7);
+    if (step13 === stepGB) stepTypeGB.push(loc.orphan13);
     if (step16 === stepGB) stepTypeGB.push(loc.quanbukaoxing);
     if (stepkd == stepGB) stepTypeGB.push(loc.zuhelongxing);
-    output += ` （` + stepTypeGB.join("／") + `）\n`;
+    output += ` ${loc.brace_left}${stepTypeGB.join(loc.slash)}${loc.brace_right}\n`;
     let brief = output;
     output += table_head + table + table_tail;
     postMessage({ output, brief });
@@ -211,11 +211,11 @@ function GBStep(tiles, tcnt, full_tcnt, subtiles, subcnt, step, save, step13, dv
         if (step7 === -1) {
             ++cnt;
             if (dvd7 === undefined) dvd7 = PairOutput(tiles);
-            odvd.push(`<div class="waiting-brief">${loc.qiduixing}${loc.colon}</div><div class="card-container">${getWinningLine(dvd7)}</div>`);
+            odvd.push(`<div class="waiting-brief">${loc.pair7}${loc.colon}</div><div class="card-container">${getWinningLine(dvd7)}</div>`);
         }
         if (step13 === -1) {
             ++cnt;
-            odvd.push(`<div class="waiting-brief">${loc.shisanyaoxing}${loc.colon}</div><div class="card-container">${getWinningLine(dvd13)}</div>`);
+            odvd.push(`<div class="waiting-brief">${loc.orphan13}${loc.colon}</div><div class="card-container">${getWinningLine(dvd13)}</div>`);
         }
         if (step16 === -1) {
             const ots = Bukao16Output(tiles);
@@ -232,9 +232,9 @@ function GBStep(tiles, tcnt, full_tcnt, subtiles, subcnt, step, save, step13, dv
         if (step === -1) {
             cnt += dvd.cnt;
             const ots = WinOutput(tiles, full_tcnt, dvd.dvd, Math.max(10 - odvd.length, 1));
-            odvd = [...ots.map((a) => `<div class="waiting-brief">${loc.yibanxing}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`), ...odvd];
+            odvd = [...ots.map((a) => `<div class="waiting-brief">${loc.normal}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`), ...odvd];
         }
-        output += loc.hepaichaijie + ": \n";
+        output += loc.windvd + ": \n";
         output += `<div class="win-grid">${odvd.join("")}</div>`;
         if (odvd.length < cnt) output += `${loc.windvd_else_head} ${cnt - odvd.length} ${loc.windvd_else_tail}`;
     }
@@ -252,16 +252,16 @@ function GBStep(tiles, tcnt, full_tcnt, subtiles, subcnt, step, save, step13, dv
 function TWStep(tiles, tcnt, full_tcnt, subtiles, subcnt, step, save, dvd) {
     let table = "";
     let stepTW = step;
-    table += `<tr><td style="padding-left: 0px;">${loc.yibanxing}${loc.colon}</td><td>${getWaitingType(step)}</td></tr>`;
+    table += `<tr><td style="padding-left: 0px;">${loc.normal}${loc.colon}</td><td>${getWaitingType(step)}</td></tr>`;
     postMessage({ output: table_head + table + table_tail });
     let step13 = Infinity, step16 = Infinity, stepnico = Infinity;
     if (full_tcnt === 17 && subcnt === 0) {
         stepnico = NiconicoStep(tiles);
-        table += `<tr><td style="padding-left: 0px;">${loc.niconicoxing}${loc.colon}</td><td>${getWaitingType(stepnico)}</td></tr>`;
+        table += `<tr><td style="padding-left: 0px;">${loc.niconico}${loc.colon}</td><td>${getWaitingType(stepnico)}</td></tr>`;
         stepTW = Math.min(stepTW, stepnico);
         postMessage({ output: table_head + table + table_tail });
         step13 = OrphanMeldStep(tiles);
-        table += `<tr><td style="padding-left: 0px;">${loc.shisanyaoxing}${loc.colon}</td><td>${getWaitingType(step13)}</td></tr>`;
+        table += `<tr><td style="padding-left: 0px;">${loc.orphan13}${loc.colon}</td><td>${getWaitingType(step13)}</td></tr>`;
         postMessage({ output: table_head + table + table_tail });
         stepTW = Math.min(stepTW, step13);
         step16 = Buda16Step(tiles);
@@ -271,11 +271,11 @@ function TWStep(tiles, tcnt, full_tcnt, subtiles, subcnt, step, save, dvd) {
     }
     let output = getWaitingType(stepTW);
     let stepTypeTW = [];
-    if (step == stepTW) stepTypeTW.push(loc.yibanxing);
-    if (stepnico == stepTW) stepTypeTW.push(loc.niconicoxing);
-    if (step13 == stepTW) stepTypeTW.push(loc.shisanyaoxing);
+    if (step == stepTW) stepTypeTW.push(loc.normal);
+    if (stepnico == stepTW) stepTypeTW.push(loc.niconico);
+    if (step13 == stepTW) stepTypeTW.push(loc.orphan13);
     if (step16 == stepTW) stepTypeTW.push(loc.shiliubudaxing);
-    output += ` （` + stepTypeTW.join("／") + `）\n`;
+    output += ` ${loc.brace_left}${stepTypeTW.join(loc.slash)}${loc.brace_right}\n`;
     let brief = output;
     output += table_head + table + table_tail;
     postMessage({ output, brief });
@@ -286,12 +286,12 @@ function TWStep(tiles, tcnt, full_tcnt, subtiles, subcnt, step, save, dvd) {
         if (stepnico === -1) {
             const ots = NiconicoOutput(tiles);
             cnt += ots.length;
-            odvd = ots.map((a) => `<div class="waiting-brief">${loc.niconicoxing}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`);
+            odvd = ots.map((a) => `<div class="waiting-brief">${loc.niconico}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`);
         }
         if (step13 === -1) {
             const ots = OrphanMeldOutput(tiles);
             cnt += ots.length;
-            odvd = [...odvd, ...ots.map((a) => `<div class="waiting-brief">${loc.shisanyaoxing}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`)];
+            odvd = [...odvd, ...ots.map((a) => `<div class="waiting-brief">${loc.orphan13}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`)];
         }
         if (step16 === -1) {
             ++cnt;
@@ -300,9 +300,9 @@ function TWStep(tiles, tcnt, full_tcnt, subtiles, subcnt, step, save, dvd) {
         if (step === -1) {
             cnt += dvd.cnt;
             const ots = WinOutput(tiles, full_tcnt, dvd.dvd, Math.max(10 - odvd.length, 1));
-            odvd = [...ots.map((a) => `<div class="waiting-brief">${loc.yibanxing}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`), ...odvd];
+            odvd = [...ots.map((a) => `<div class="waiting-brief">${loc.normal}${loc.colon}</div><div class="card-container">${getWinningLine(a)}</div>`), ...odvd];
         }
-        output += loc.hepaichaijie + ": \n";
+        output += loc.windvd + ": \n";
         output += `<div class="win-grid">${odvd.join("")}</div>`;
         if (odvd.length < cnt) output += `${loc.windvd_else_head} ${cnt - odvd.length} ${loc.windvd_else_tail}`;
     }

97a25a7e895c8c79cfe94c79979147b3139e76ae
SheauhawJang <www.zhangxiaohao.cn@foxmail.com>
2026-09-05 12:18:05 +0800

    Site updated: 2026-09-05 12:18:04

--- old_blogs/tools/mahjong-worker.js
+++ old_blogs/tools/mahjong-worker.js
@@ -1,6 +1,6 @@
-importScripts("mahjong.js?v=202609042142");
-importScripts("mahjong-score.js?v=202609042142");
-importScripts("mahjong-worker-lang.js?v=202609042142");
+importScripts("mahjong.js?v=202609051217");
+importScripts("mahjong-score.js?v=202609051217");
+importScripts("mahjong-worker-lang.js?v=202609051217");
 //console.log(JPPrintSeq.map((i) => loc_all[`JP_YAKUNAME_${i}`].ja).join("\n"));
 //console.log(Array(69).fill(0).map((_,i)=>cn_loc[`JP_YAKUNAME_${i}`]).join('\n'));
 const MAX_OUTPUT_LENGTH = 12;
